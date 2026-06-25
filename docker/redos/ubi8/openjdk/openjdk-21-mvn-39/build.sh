d1d2d6460c00ea10da0f6fe1e8953c44f16a86cc
GD-TG <ertyjr95@gmail.com>
2026-06-26 01:11:36 +0500

    first commit

---
+++ docker/redos/ubi8/openjdk/openjdk-21-mvn-39/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# Скрипт для локальной сборки образа
+source ../../../../.env
+
+docker buildx build \
+  --platform=linux/amd64 \
+  --build-context=common=../../../../../common \
+  -t "${OPENJDK_21_MVN_39_IMAGE_NAME}:${VERSION}" \
+  -t "${OPENJDK_21_MVN_39_IMAGE_NAME}:latest" \
+  -f Dockerfile \
+  --build-arg REGISTRY_REDSOFT="${REGISTRY_REDSOFT}" \
+  --build-arg BUILD_ENVIRONMENT="${BUILD_ENVIRONMENT}" \
+  --pull \
+  --load \
+  --no-cache \
+  --progress plain \
+  .
